573bfa6c9dc205d190525471f555c86f3fe75d26
Nevin <n.boyd1@yahoo.com>
2025-12-05 14:59:38 -0800

    Add Star field renderer and StarMap base layout
    Refactor starfield rendering for Sigil overlay

--- src/pages/StarMap.jsx
+++ src/pages/StarMap.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import StarFieldCanvas from "../components/StarFieldCanvas";
+import AriesHead from "../components/AriesHead";
+import ZodiacCards from "../components/ZodiacCards";
 import "./StarMap.css";
 
 export default function StarMap() {
-    return (
-        <div className="star-map-page">
-            <StarFieldCanvas />
+  const [activeSign, setActiveSign] = useState(null);
 
-            <div className="star-map-layer">
-                <h1 style={{ color: "var(--color-cloudWhite)", textAlign: "center", marginTop: "20vh" }}>
-                    Star Map Placeholder
-                </h1>
-            </div>
-        </div>
-    );
+  function handleSigilClick() {
+    setActiveSign("Aries");
+  }
+
+  return (
+    <div className="star-map-page">
+      <StarFieldCanvas />
+
+      {/* Aries Sigil Button */}
+      <AriesHead onClick={handleSigilClick} />
+
+      {/* Cards appear after selecting a sign */}
+      {activeSign && (
+        <ZodiacCards 
+          sign={activeSign} 
+          onClose={() => setActiveSign(null)} 
+        />
+      )}
+    </div>
+  );
 }
