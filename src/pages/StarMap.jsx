46fe12be2421cb1cdf9ecbdef9e8f8fac45c5c83
Nevin <n.boyd1@yahoo.com>
2025-12-06 07:15:54 -0800

    Add Libra sigil, UI placement, lore support

--- src/pages/StarMap.jsx
+++ src/pages/StarMap.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import StarFieldCanvas from "../components/StarFieldCanvas";
 import AriesHead from "../components/AriesHead";
 import Cancer from "../components/Cancer";
+import Libra from "../components/Libra";
 import ZodiacCards from "../components/ZodiacCards";
 import "./StarMap.css";
 
@@ -16,15 +17,22 @@ export default function StarMap() {
     setActiveSign("Cancer");
   }
 
+  function handleLibraClick() {
+    setActiveSign("Libra")
+  }
+
   return (
     <div className="star-map-page">
       <StarFieldCanvas />
 
-      {/* Aries Sigil Button */}
+      {/* Aries Sigil */}
       <AriesHead onClick={handleSigilClick} />
       
       {/* Cancer Sigil */}
       <Cancer onClick={handleCancerClick} />
+
+      {/* Libra Sigil */}
+      <Libra onClick={handleLibraClick} />
 
       {/* Cards appear after selecting a sign */}
       {activeSign && (
